fb7cbea3719eac9c887c6ef0b447344696d9ead3
RohitMunjal25 <mobilemunjal@gmail.com>
2026-02-14 13:59:59 +0530

    video changed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function openVideo(type) {
   if(type === "editor") {
     frame.src = "https://www.youtube.com/embed/ipj8uJTzgqU?si=LJ3eybBPaaemYhhT";
   } else {
-    frame.src = "https://www.youtube.com/embed/YOUR_TEAM_VIDEO_ID";
+    frame.src = "https://youtube.com/embed/rb74wHQK3W8?feature=share";
   }
 
   modal.style.display = "flex";
